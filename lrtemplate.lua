9b526426c55a7186de43efb94f17a5e8558765fc
Jarno Heikkinen <jarnoh@matlock.local>
2013-10-19 00:49:14 +0300

    remove some lua methods to harden lrtemplate loading

--- lrtemplate.lua
+++ lrtemplate.lua
@@ -6,9 +6,13 @@ end
 
 local function loadlrtemplate(s)
 	if s:sub(1,5)=="s = {" then
+
 		s = "return "..s:sub(4)
-		local c = loadstring(s) -- TODO secure this
-		return c()
+		
+		-- add some level for hacking -- TODO secure this
+		s = "local _G=nil local print=nil local setfenv=nil local require=nil local io=nil local os=nil local coroutine=nil local debug=nil local table=nil local loadfile=nil local loadstring=nil local load=nil local dofile=nil local setmetatable=nil local xpcall=nil local pcall=nil local package=nil "..s
+		local c = loadstring(s)
+		return c and c()
 	end
 
 	return nil -- not lrtemplate
